01485cbc2de26031192c8673e9e17e5abb25704e
zhitingli12 <zhitingli54678@gmail.com>
2024-04-11 15:10:50 -0400

    main

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-def encoder(numbers):
-    output_list = []
-    output = ""
-    for i in numbers:
-        output_list.append(int(i) + 3)
-    for i in output_list:
-        output = output + str(i)
-    return output
+if __name__ == '__main__':
+    def menu():
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            password = input("Please enter your password to encode:")
+            encoded_pass = encoder(password)
+            print("Your password has been encoded and stored.")
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+    def encoder(numbers):
+        output_list = []
+        output = ""
+        for i in numbers:
+            output_list.append(int(i) + 3)
+        for i in output_list:
+            output = output + str(i)
+        return output
+
+    def decoder(numbers):
+        outputt_list = []
+        outputt = ""
+        for i in numbers:
+            outputt_list.append(int(i) - 3)
+        for i in outputt_list:
+            outputt = outputt + str(i)
+        return outputt
